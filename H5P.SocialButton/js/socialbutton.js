9dc61c5913d01efa6b5f5891dbbdba7c91fb7f8c
Gunhild Griff With <griff.with@gmail.com>
2013-08-23 13:19:04 +0200

    Starting to take form. Semantic not working!

--- H5P.SocialButton/js/socialbutton.js
+++ H5P.SocialButton/js/socialbutton.js
@@ -41,47 +41,34 @@ H5P.SocialButton = function(params, contentId) {
     "placement": "left",
     "dataTheme": "default",
     "dataNetworks": {
-      "Facebook": true,
-      "Twitter": true,
-      "Dribbble": true,
-      "Socl": true,
-      "Youtube": true,
-      "Vimeo": true,
-      "Google Plus": true,
-      "Pinterest": true,
-      "LinkedIn": true,
-      "Flickr": true,
-      "Blogger": true,
-      "GitHub": true,
-      "DeviantArt": true,
-      "Skype": true,
-      "Steam": true,
-      "Wordpress": true,
-      "Yahoo": true,
-      "eMail": true
+      "facebook": true,
+      "facebookHandle": "exFacebookHandler",
+      "twitter": true,
+      "dribbble": false,
+      "socl": false,
+      "youtube": true,
+      "vimeo": true,
+      "google": true,
+      "pinterest": false,
+      "linkedin": true,
+      "flickr": true,
+      "blogger": false,
+      "github": true,
+      "deviantart": false,
+      "skype": false,
+      "steam": false,
+      "wordpress": false,
+      "yahoo": false,
+      "email": true
     },
-    "dataImageType": true,
-    "dataPicture": true,
-    "dataYYYHandle": {
-      "data_name_handle": true,
-      "data_name2_handle": true
-    },
+    "dataImageType": "",
+    "dataPicture": "",
     "dataOrientation": "arc",
-    "dataGap": 30,
-    "dataArcStart": 30,
+    "dataGap": 70,
+    "dataArcStart": 0,
     "dataArcLength": 360,
-    "dataRadius": 20
+    "dataRadius": 80
   };
-
-  //<div class="classysocial rif (params.dataNetworks.Twitter) {ight" 
-  //  data-arc-length="360" 
-  //  data-image-type="facebook" 
-  //  data-picture="picozu" 
-  //  data-facebook-handle="picozu" 
-  //  data-twitter-handle="picozu_editor" 
-  //  data-email-handle="office@picozu.net" 
-  //  data-networks="facebook,twitter,email">
-
 
   // setting all the paramters into one variable
   var params = $.extend({}, defaults, params);
@@ -102,87 +89,32 @@ H5P.SocialButton = function(params, contentId) {
 
     networks = " ";
     handlers = " ";
-    if (params.dataNetworks.Facebook) {
-      networks = "facebook";
-      handlers = ' data-facebook-handle="examleHandle" ';
-    }
-    if (params.dataNetworks.Twitter) {
-      networks = (networks == "") ? "twitter" : networks + ",twitter";
-      handlers = handlers + 'data-twitter-handle="examleHandle" ';
-    }
-    if (params.dataNetworks.Dribbble) {
-      networks = (networks == "") ? "dribbble" : networks + ",dribbble";
-      handlers = handlers + 'data-dribbble-handle="examleHandle" ';
-    }
-    if (params.dataNetworks.Socl) {
-      networks = (networks == "") ? "socl" : networks + ",socl";
-      handlers = handlers + 'data-socl-handle="examleHandle" ';
-    }
-    if (params.dataNetworks.Youtube) {
-      networks = (networks == "") ? "youtube" : networks + ",youtube";
-      handlers = handlers + 'data-youtube-handle="examleHandle" ';
-    }
-    if (params.dataNetworks.Vimeo) {
-      networks = (networks == "") ? "vimeo" : networks + ",vimeo";
-      handlers = handlers + 'data-vimeo-handle="examleHandle" ';
-    }
-    if (params.dataNetworks.GooglePlus) { // toLowerCase, instagram
-      networks = (networks == "") ? "google" : networks + ",google";
-      handlers = handlers + 'data-google-handle="examleHandle" ';
-    }
-    if (params.dataNetworks.Pinterest) {
-      networks = (networks == "") ? "pinterest" : networks + ",pinterest";
-      handlers = handlers + 'data-pinterest-handle="examleHandle" ';
-    }
-    if (params.dataNetworks.LinkedIn) {
-      networks = (networks == "") ? "linkedin" : networks + ",linkedin";
-      handlers = handlers + 'data-linkedin-handle="examleHandle" ';
-    }
-    if (params.dataNetworks.Flickr) {
-      networks = (networks == "") ? "flickr" : networks + ",flickr";
-      handlers = handlers + 'data-flickr-handle="examleHandle" ';
-    }
-    if (params.dataNetworks.Blogger) {
-      networks = (networks == "") ? "blogger" : networks + ",blogger";
-      handlers = handlers + 'data-blogger-handle="examleHandle" ';
-    }
-    if (params.dataNetworks.GitHub) {
-      networks = (networks == "") ? "github" : networks + ",github";
-      handlers = handlers + 'data-github-handle="examleHandle" ';
-    }
-    if (params.dataNetworks.DeviantArt) {
-      networks = (networks == "") ? "deviantart" : networks + ",deviantart";
-      handlers = handlers + 'data-deviantart-handle="examleHandle" ';
-    }
-    if (params.dataNetworks.Skype) {
-      networks = (networks == "") ? "skype" : networks + ",skype";
-      handlers = handlers + 'data-skype-handle="examleHandle" ';
-    }
-    if (params.dataNetworks.Steam) {
-      networks = (networks == "") ? "steam" : networks + ",steam";
-      handlers = handlers + 'data-steam-handle="examleHandle" ';
-    }
-    if (params.dataNetworks.Wordpress) {
-      networks = (networks == "") ? "wordpress" : networks + ",wordpress";
-      handlers = handlers + 'data-wordpress-handle="examleHandle" ';
-    }
-    if (params.dataNetworks.Yahoo) {
-      networks = (networks == "") ? "yahoo" : networks + ",yahoo";
-      handlers = handlers + 'data-yahoo-handle="examleHandle" ';
-    }
-    if (params.dataNetworks.eMail) {
-      networks = (networks == "") ? "email" : networks + ",email";
-      handlers = handlers + 'data-email-handle="examleHandle" ';
+    for (var property in params.dataNetworks) {
+      if (property.indexOf("Handle") == -1) {
+        if (params.dataNetworks[property]) {
+          networks = (networks == " ") ? property : networks + "," + property;
+          handlers = handlers + 'data-' + property + '-handle';
+        }
+        addNetwork = params.dataNetworks[property];
+      } else {
+        if (addNetwork) {
+          handlers = handlers + '="' + params.dataNetworks[property] + '" ';
+        }
+      }
     }
 
     //Adding the Social Button
-    console.log("test: " + handlers);
     $myDom.html('<div class="classysocial ' + params.placement +
+            '" data-theme="' + params.dataTheme +
+            '" data-image-type="' + params.dataImageType +
+            '" data-picture="' + params.dataPicture +
             '" data-orientation="' + params.dataOrientation +
-            '" data-arc-length="' + params.dataArcLength +
-            '"' + handlers +
+            '" data-arc-length=' + params.dataArcLength +
+            ' data-gap=' + params.dataGap +
+            ' data-arc-start=' + params.dataArcStart +
+            ' data-radius=' + params.dataRadius +
+            handlers +
             ' data-networks="' + networks +
-            //'" data-image-type="facebook" data-picture="picozu"></div>');
             '"></div>');
 
     $(function() {
