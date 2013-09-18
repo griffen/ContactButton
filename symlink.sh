4ff01d8779a1076e8591298c206c07ff62648fd9
Gunhild Griff With <griff.with@gmail.com>
2013-09-18 09:50:29 +0200

    test

---
+++ symlink.sh
@@ -0,0 +1,3 @@
+sudo rm -r /var/www/drupal6/sites/default/files/h5p/libraries/H5P.ContactButton-1.0
+sudo ln -s /home/griffen/dev/social_button/H5P.ContactButton /var/www/drupal6/sites/default/files/h5p/libraries/H5P.ContactButton-1.0
+ls -all /var/www/drupal6/sites/default/files/h5p/libraries/
